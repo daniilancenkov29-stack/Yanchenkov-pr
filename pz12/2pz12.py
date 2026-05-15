be53ee2949b9b26f70d1313c00c05f534f45ca35
daniil21 <daniilancenkov29@gmail.com>
2026-05-15 18:23:17 +0300

    pz6

--- pz12/2pz12.py
+++ pz12/2pz12.py
@@ -1,8 +1,9 @@
-# Для каждой строки матрицы с нечетным номером найти среднее арифметическое ее 
-#элементов.
+# Для каждой строки матрицы с нечетным номером найти среднее арифметическое ее элементов.
 import random
-matrix = [[random.randint(0, 20) for _ in range(4)] for _ in range(3)]
+strok = int(input("Введите количество строк: "))
+stolb = int(input("Введите количество столбцов: "))
+matrix = [[random.randint(0, 20) for i in range(strok)] for i in range(stolb)]
 print("Матрица:", matrix)
-odd_rows = list(filter(lambda i: i % 2 != 0, range(len(matrix))))
-averages = list(map(lambda i: sum(matrix[i]) / len(matrix[i]), odd_rows))
-print("Среднее арифметическое для строк с нечетным номером:", averages)
+nechet = list(filter(lambda s: s % 2 != 0, range(len(matrix))))
+sred = list(map(lambda s: sum(matrix[s]) / len(matrix[s]), nechet))
+print("Среднее арифметическое для строк с нечетным номером:", sred)
--- pz12/pz12.py
+++ pz12/pz12.py
@@ -1,9 +1,10 @@
 #В матрице найти отрицательные элементы, сформировать из них новый массив. 
 #Вывести размер полученного массива
 import random
-matrix = [[random.randint(-10, 10) for _ in range(4)] for _ in range(3)]
+strok = int(input("Введите количество строк: "))
+stolb = int(input("Введите количество столбцов: "))
+matrix = [[random.randint(-10, 10) for i in range(strok)] for i in range(stolb)]
 print("Матрица:", matrix)
-negative_elements = list(filter(lambda x: x < 0, [num for row in matrix for num in row]))
+negative_elements = list(filter(lambda x: x < 0, [num for strok in matrix for num in strok]))
 print("Отрицательные элементы:", negative_elements)
 print("Размер массива:", len(negative_elements))
-
